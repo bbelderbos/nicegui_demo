b0a9d97ef89498e2235fea081a298c2af2e746ba
Bob Belderbos <bobbelderbos@gmail.com>
2025-07-29 16:38:11 +0200

    handle form submit

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def journal_timeline():
             )
 
 
+def handle_add_entry(title_input, content_input, tags_input):
+    raw_tags = tags_input.value.split(",")
+    tags = [tag.strip() for tag in raw_tags if tag.strip()]
+    db.add_entry(title=title_input.value, content=content_input.value, tags=tags)
+    ui.notify("Entry added", type="positive")
+    # reset form
+    title_input.set_value("")
+    content_input.set_value("")
+    tags_input.set_value("")
+
+
 with ui.column().classes("w-full max-w-screen-xl mx-auto"):
     with ui.row().classes("w-full flex-nowrap gap-4"):
         with ui.column().classes("w-6/12"):
@@ -29,7 +40,10 @@ with ui.column().classes("w-full max-w-screen-xl mx-auto"):
                 new_title = ui.input("Title").classes("w-full")
                 new_content = ui.textarea("Content").classes("w-full")
                 new_tags = ui.input("Tags (comma-separated)").classes("w-full")
-                ui.button("Add Entry")
+                ui.button(
+                    "Add Entry",
+                    on_click=lambda: handle_add_entry(new_title, new_content, new_tags),
+                )
 
 
 ui.run()
